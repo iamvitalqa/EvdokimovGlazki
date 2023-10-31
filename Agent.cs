dad89d2ebc0f4d3c5753cc4d6fb2f23309fe6b99
vitalqa <sozima106@gmail.com>
2023-10-31 17:54:07 +0500

    11 laba

--- Agent.cs
+++ Agent.cs
@@ -41,5 +41,14 @@ namespace EvdokimovGlazki
         public virtual ICollection<ProductSale> ProductSale { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Shop> Shop { get; set; }
+
+        public string AgentTypeString
+        {
+            get
+            {
+                return AgentType.Title;
+            }
+        }
     }
 }
+
--- ServicePage.xaml.cs
+++ ServicePage.xaml.cs
@@ -25,11 +25,73 @@ namespace EvdokimovGlazki
             InitializeComponent();
             var currentServices = Evdokimov_glazkiEntities.GetContext().Agent.ToList();
             ServiceListView.ItemsSource = currentServices;
+            ComboSortSearch.SelectedIndex = 0;
+            ComboTypeSearch.SelectedIndex = 0;
+            UpdateServices();
+        }
+
+        private void UpdateServices()
+        {
+            var currentServices = Evdokimov_glazkiEntities.GetContext().Agent.ToList();
+            currentServices = currentServices.Where(p => (p.Title.ToLower().Contains(TBoxSearch.Text.ToLower()) || p.Email.ToLower().Contains(TBoxSearch.Text.ToLower()) || p.Phone.Replace("+7", "8").Replace("(", "").Replace(") ","").Replace("+7","7").Replace(" ","").Replace("-","").Contains(TBoxSearch.Text.Replace("+7","8").Replace("(", "").Replace(") ", "").Replace(" ", "").Replace("-", "")))).ToList();
+
+            if (ComboSortSearch.SelectedIndex == 0)
+                currentServices = currentServices.OrderBy(p => p.Title).ToList();
+            if (ComboSortSearch.SelectedIndex == 1)
+                currentServices = currentServices.OrderByDescending(p => p.Title).ToList();
+            if (ComboSortSearch.SelectedIndex == 2)
+                currentServices = currentServices.OrderBy(p => p.Title).ToList();
+            if (ComboSortSearch.SelectedIndex == 3)
+                currentServices = currentServices.OrderByDescending(p => p.Title).ToList();
+            if (ComboSortSearch.SelectedIndex == 4)
+                currentServices = currentServices.OrderBy(p => p.Priority).ToList();
+            if (ComboSortSearch.SelectedIndex == 5)
+                currentServices = currentServices.OrderByDescending(p => p.Priority).ToList();
+
+            if (ComboTypeSearch.SelectedIndex == 0)
+                currentServices = currentServices.Where(p => (p.AgentTypeID >= 1 && p.AgentTypeID <=6)).ToList();
+            if (ComboTypeSearch.SelectedIndex == 1)
+                currentServices = currentServices.Where(p => (p.AgentTypeID == 1)).ToList();
+            if (ComboTypeSearch.SelectedIndex == 2)
+                currentServices = currentServices.Where(p => (p.AgentTypeID == 2)).ToList();
+            if (ComboTypeSearch.SelectedIndex == 3)
+                currentServices = currentServices.Where(p => (p.AgentTypeID == 3)).ToList();
+            if (ComboTypeSearch.SelectedIndex == 4)
+                currentServices = currentServices.Where(p => (p.AgentTypeID == 4)).ToList();
+            if (ComboTypeSearch.SelectedIndex == 5)
+                currentServices = currentServices.Where(p => (p.AgentTypeID == 5)).ToList();
+            if (ComboTypeSearch.SelectedIndex == 6)
+                currentServices = currentServices.Where(p => (p.AgentTypeID == 6)).ToList();
+
+
+
+            ServiceListView.ItemsSource = currentServices;
+
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Manager.MainFrame.Navigate(new AddEditPage());
+        }
+
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateServices();
+        }
+
+        private void ComboTypeSearch_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateServices();
+        }
+
+        private void ComboSortSearch_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateServices();
+        }
+
+        private void TBoxSearch_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 }
